faa3a16ac99bb323908c6c1df039d0d77d6f1433
Jacek <jacekwegr@gmail.com>
2022-04-25 00:39:51 +0200

    small refactor

--- src/useWindowWidth.js
+++ src/useWindowWidth.js
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
 
-function useWindowWidth() {
+const useWindowWidth = () => {
   const [width, setWidth] = useState(window.innerWidth);
   useEffect(() => {
-    function handleResize() {
+    const handleResize = () => {
       setWidth(window.innerWidth);
-    }
+    };
     window.addEventListener("resize", handleResize);
     handleResize();
 
     return () => window.removeEventListener("resize", handleResize);
   }, []);
   return width;
-}
+};
 
 export default useWindowWidth;
